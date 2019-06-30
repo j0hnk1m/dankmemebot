1aebfe4c8f58af413d8ca6443639ace8abc8d5ea
John <kim.john228@gmail.com>
2019-06-30 14:40:06 -0700

    cleaned up some code

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import praw
 import pandas as pd
 import datetime as dt
+import access_api
 
 ACCESS_TOKEN = 'FC0tkuO4MU0Wjg'
 ACCESS_TOKEN_SECRET = 'n4II27rRz_M_fqbl242zmcyayvY'
@@ -9,4 +10,4 @@ ACCESS_TOKEN_SECRET = 'n4II27rRz_M_fqbl242zmcyayvY'
 
 def get_memes():
     reddit = praw.Reddit(client_id=ACCESS_TOKEN, client_secret=ACCESS_TOKEN_SECRET, user_agent='DankMemeBot',
-                         username=USERNAME, password=PASSWORD)
+                         username=access_api.USERNAME, password=access_api.PASSWORD)
